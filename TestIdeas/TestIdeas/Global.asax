dd3d6d95e1d7e94e685eb728f175bde3605f81c5
Zohrab Adrian <zdastkhan@gmail.com>
2020-05-23 03:06:44 +0100

    initial VS solution

---
+++ TestIdeas/TestIdeas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestIdeas.MvcApplication" Language="C#" %>
---
+++ TestIdeas/TestIdeas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestIdeas.Startup))]
+namespace TestIdeas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
