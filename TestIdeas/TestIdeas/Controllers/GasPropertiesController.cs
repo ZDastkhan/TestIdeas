5c8175b614c906cef9a4b0856263f2d06cb2ba05
Zohrab Adrian <zdastkhan@gmail.com>
2020-05-24 16:15:37 +0100

    create a simple view for gas properties

---
+++ TestIdeas/TestIdeas/Controllers/GasPropertiesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestIdeas.Models;
+
+namespace TestIdeas.Controllers
+{
+    public class GasPropertiesController : Controller
+    {
+        [HttpGet]
+        public ActionResult GasProperties()
+        {
+            var model = new GasPropertiesModel();
+            return View(model);
+        }
+
+    }
+}
---
+++ TestIdeas/TestIdeas/Models/GasPropertiesModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestIdeas.Models
+{
+    public class GasPropertiesModel
+    {
+        public string Title { get; set; }
+        public double GasSG { get; set; }
+        public double GasH2S { get; set; }
+        public double GasCO2 { get; set; }
+        public double GasN2 { get; set; }
+        public string GasZFactor { get; set; }
+        public double Pressure { get; set; }
+        public double Temperature { get; set; }
+
+        public string Message { get; set; }
+        public string IsSuccessful { get; set; }
+
+        public GasPropertiesModel()
+        {
+            GasSG = 0.7;
+            Message = string.Empty;
+            Pressure = 2500;
+            Temperature = 180;
+        }
+    }
+}
