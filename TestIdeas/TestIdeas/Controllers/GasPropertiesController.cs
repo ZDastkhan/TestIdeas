09c587674c1ef611fec3e733d468030cf41ead65
Zohrab Dastkhan <zdastkhan@yahoo.com>
2020-06-05 03:30:35 +0100

    Revert "cleanup code"
    This reverts commit c5c0352a5a8070f5b628251e71fe7add9fd4afdb.

---
+++ TestIdeas/TestIdeas/Controllers/GasPropertiesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using TestIdeas.Helpers;
+using TestIdeas.Models;
+
+namespace TestIdeas.Controllers
+{
+    public class GasPropertiesController : Controller
+    {
+        [HttpGet]
+        public ActionResult GasProperties()
+        {
+            var model = new GasPropertiesModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CalculateGasProperties(FormCollection fc)
+        {
+            var modelData = new GasPropertiesModel();
+
+            var temp = ControllerHelpers.PrintVariables(fc);
+
+            modelData.IsSuccessful = true;
+            modelData.Message = "";
+            var outputJson = JsonConvert.SerializeObject(modelData);
+            return Content(outputJson);
+        }
+    }
+}
